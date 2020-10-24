3443ebb8953869bfdb22dc267a3c3c9226ae23fa
Jawahar Lal <jawahar.ajg@gmail.com>
2020-10-24 19:55:01 +0530

    Refactoring part2

--- src/test/java/com/apttus/pageobjects/HomePage.java
+++ src/test/java/com/apttus/pageobjects/HomePage.java
@@ -1,5 +1,6 @@
 package com.apttus.pageobjects;
 
+import com.apttus.util.WebUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,10 +10,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
     public TshirtCatPage clickCatTshirts(WebDriver driver) {
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div/ul/li/a[@title='T-shirts']")));
-        WebElement tshirtLabel = driver.findElement(By.xpath("//div/ul/li/a[@title='T-shirts']"));
-        tshirtLabel.click();
+        WebUtil.waitForElementVisible(driver, By.xpath("//div/ul/li/a[@title='T-shirts']"));
+        WebUtil.click(driver, By.xpath("//div/ul/li/a[@title='T-shirts']"));
 
         return PageFactory.initElements(driver, TshirtCatPage.class);
     }
